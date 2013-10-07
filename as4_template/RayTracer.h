3378fab751e0830682decb6c3218276caa3b9e68
jxb532 <jxb532@case.edu>
2013-10-07 18:02:31 -0400

    added ray header file

---
+++ as4_template/RayTracer.h
@@ -0,0 +1,6 @@
+/* Wes Rupert - wesrupert@outlook.com (wkr3)  *
+ * Josh Braun - jxb532@case.edu (jxb532)      *
+ * Case Western Reserve University - EECS 366 *
+ * 10/07/2013 - Assignment 4                  */
+
+int intersect_triangle(double orig[3], double dir[3], double vert0[3], double vert1[3], double vert2[3], double* t, double* u, double* v);
